fce112737c22bff9c6e5c67c48b5b81175c32e75
zynwk <108486945+zynwk@users.noreply.github.com>
2022-12-12 14:50:19 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,59 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var canvas;
+var player, playerBase, playerArcher;
+var baseimage;
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.png");
+  baseimage = loadImage("./assets/base.png");
+  playerimage = loadImage("./assets/player.png");
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  angleMode(DEGREES);
+
+  var options = {
+    isStatic: true
+  };
+
+  playerBase = Bodies.rectangle(200,350,180,150, options);
+  World.add(world, playerBase);
+
+  player = Bodies.rectangle(250, playerBase.position.y - 160,50,180,options);
+  World.add(world, player);
+  //criar corpo da base do jogador
+
+  //criar corpo do jogador
+
+
+
+}
+
+function draw() {
+  background(backgroundImg);
+
+  //exibir a imagem do jogador usando a função image()
+  image(baseimage,playerBase.position.x,playerBase.position.y,180,150)
+  image(playerimage,player.position.x,player.position.y,50,180)
+
+  //exibir a imagem da base do jogador usando a função image()
+
+
+  Engine.update(engine);
+
+  // Title
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("ARQUEIRO ÉPICO", width / 2, 100);
+}
